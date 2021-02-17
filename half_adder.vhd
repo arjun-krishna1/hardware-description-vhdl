97f064a57e40dca6ff8d6bd9aad99b1010d5580c
arjun_krishna <45014214+arjun-krishna1@users.noreply.github.com>
2021-02-16 19:47:35 -0500

    add half adder circuit description

---
+++ half_adder.vhd
@@ -0,0 +1,20 @@
+-- half adder circuit
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity half_adder is
+  Port( 
+        a       : in std_logic;
+        b       : in std_logic;
+        sum     : out std_logic;
+        carry 	: out std_logic
+  );
+  
+  end half_adder;
+
+  architecture Behavioral of half_adder is
+    begin
+      sum <= a xor b ;
+          carry <= a and b;
+	end Behavioral;
